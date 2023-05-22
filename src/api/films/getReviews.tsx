af165cf5359951003648cd19022d994c903b3d5b
Hugh <hav13@uclive.ac.nz>
2023-05-22 21:11:28 +1200

    good sesh

--- src/api/films/getReviews.tsx
+++ src/api/films/getReviews.tsx
@@ -11,3 +11,33 @@ export async function getReviews(filmId: string) {
     return null;
   }
 }
+
+export async function getSimilarFilms(
+  genreId: number,
+  directorId: number,
+  currentFilmId: number
+) {
+    try {
+      const url1 = `${API_URL}/films?genreIds=${genreId}`
+      const response1 = await axios.get(url1);
+      const filmsWithSameGenre = response1.data.films;
+
+      const url2 = `${API_URL}/films?directorId=${directorId}`
+      const response2 = await axios.get(url2);
+      const filmsWithSameDirector = response2.data.films;
+      
+      const similarFilms = [
+        ...filmsWithSameGenre,
+        ...filmsWithSameDirector,
+      ].filter(
+        (film, index, self) =>
+          film.filmId !== currentFilmId &&
+          index === self.findIndex((f) => f.filmId === film.filmId)
+      );
+      
+      return similarFilms;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
--- src/page/selectedFilm.tsx
+++ src/page/selectedFilm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { getFilm } from '../api/films/getFilms';
 import { Film } from '../types/film';
 import { Review } from '../types/review';
@@ -7,7 +7,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {API_URL} from "../api/CONSTANTS";
 import { Genre } from '../types/genre'
 import { getGenres } from '../api/films/getGenres'
-import { getReviews } from '../api/films/getReviews'
+import { getReviews, getSimilarFilms} from '../api/films/getReviews'
+
 
 
 function SelectedFilm() {
@@ -15,6 +16,8 @@ function SelectedFilm() {
   const [film, setFilm] = useState<Film | null>(null);
   const [genres, setGenres] = useState([] as Genre[])
   const [reviews, setReviews] = useState([] as Review[])
+  const [similarFilms, setSimilarFilms] = useState<Film[] | null>(null);
+
   useEffect(() => {
     async function fetchFilm() {
       if (id) {
@@ -22,20 +25,37 @@ function SelectedFilm() {
         setFilm(filmData);
       }
     }
+  
     async function fetchGenres() {
-      const response = await getGenres()
+      const response = await getGenres();
       setGenres(response);
     }
+  
     async function fetchReviews() {
       if (id) {
-      const response = await getReviews(id);
-      setReviews(response);
+        const response = await getReviews(id);
+        setReviews(response);
       }
     }
-    fetchReviews();
-    fetchGenres();
+  
     fetchFilm();
+    fetchGenres();
+    fetchReviews();
   }, [id]);
+  
+  useEffect(() => {
+    async function fetchSimilarFilms() {
+      if (film) {
+        const response = await getSimilarFilms(film.genreId, film.directorId, film.filmId);
+        setSimilarFilms(response);
+      }
+    }
+  
+    fetchSimilarFilms();
+  }, [film]);
+  
+
+
   if (film === null) {
     return (
       <div>
@@ -61,7 +81,7 @@ function SelectedFilm() {
             </div>
           </div>
         </div>
-        
+  
         <div className="col-md-6 offset-md-3 mb-3">
           <div className="card" style={{ width: '30rem' }}>
             <h5 className="card-title">Director info</h5>
@@ -71,69 +91,68 @@ function SelectedFilm() {
             </div>
           </div>
         </div>
-        
+  
         <div className="col-md-6 offset-md-3 mb-3">
           <div className="card" style={{ width: '30rem' }}>
             <h5 className="card-title">Reviews and Ratings</h5>
             <div className="card-body">
-            <p className="rating">Rating: <span className="text-success ">{film.rating}</span>/10</p>
-            <p>Number of Reviews: {reviews.length}</p>
-
-          {/* Loop through reviews and create cards */}
-          {reviews.map((review, index) => (
-          <div className="card mb-3" key={index}>
-            <div className="card-body">
-              <h6 className="card-subtitle">Review #{index + 1}</h6>
-              <p className="card-text">reviewer: {review.reviewerFirstName} {review.reviewerLastName}</p>                         
-              <img src={`${API_URL}/users/${review.reviewerId}/image`} alt="Photo" width={40} onError={e => (e.target as HTMLImageElement).src = "https://avatar.vercel.sh/cookie"} />
-              <p className="card-text">Review: {review.review ? review.review : "N/A"}</p>
-              <p className="card-text">{review.reviewerFirstName} Reviewed {film.title} a {review.rating}/10</p>
-              <p className="card-text">Time of Review: {new Date(review.timestamp).toLocaleString()}</p>
-            
-            </div>
-          </div>
-          ))}
-
+              <p className="rating">Rating: <span className="text-success ">{film.rating}</span>/10</p>
+              <p>Number of Reviews: {reviews.length}</p>
+  
+              {/* Loop through reviews and create cards */}
+              {reviews.map((review, index) => (
+                <div className="card mb-3" key={index}>
+                  <div className="card-body">
+                    <h6 className="card-subtitle">Review #{index + 1}</h6>
+                    <p className="card-text">reviewer: {review.reviewerFirstName} {review.reviewerLastName}</p>
+                    <img src={`${API_URL}/users/${review.reviewerId}/image`} alt="Photo" width={40} onError={e => (e.target as HTMLImageElement).src = "https://avatar.vercel.sh/cookie"} />
+                    <p className="card-text">Review: {review.review ? review.review : "N/A"}</p>
+                    <p className="card-text">{review.reviewerFirstName} Reviewed {film.title} a {review.rating}/10</p>
+                    <p className="card-text">Time of Review: {new Date(review.timestamp).toLocaleString()}</p>
+                  </div>
+                </div>
+              ))}
+  
             </div>
           </div>
         </div>
-
-
-
-        <div className="col-md-6 offset-md-3 mb-3">
-          <div className="card" style={{ width: '30rem' }}>
+  
+        <div className="col-md-12 mb-3">
+          <div className="card" style={{ width: '100%' }}>
             <h5 className="card-title">Similar Films</h5>
             <div className="card-body">
-          {reviews.map((review, index) => (
-          <div className="card mb-3" key={index}>
-            <div className="card-body">
-              <h6 className="card-subtitle">Review #{index + 1}</h6>
-              <p className="card-text">reviewer: {review.reviewerFirstName} {review.reviewerLastName}</p>                         
-              <img src={`${API_URL}/users/${review.reviewerId}/image`} alt="Photo" width={40} onError={e => (e.target as HTMLImageElement).src = "https://avatar.vercel.sh/cookie"} />
-              <p className="card-text">Review: {review.review ? review.review : "N/A"}</p>
-              <p className="card-text">{review.reviewerFirstName} Reviewed {film.title} a {review.rating}/10</p>
-              <p className="card-text">Time of Review: {new Date(review.timestamp).toLocaleString()}</p>
-            
-            </div>
-          </div>
-          ))}
-
+              {similarFilms === null ? (
+                <p>Loading similar films...</p>
+              ) : similarFilms.length > 0 ? (
+                <div className="row">
+                  {similarFilms.slice(0, 3).map((similarFilm) => (
+                    <div className="col-md-2" key={similarFilm.filmId}>
+                      <div className="card mb-3">
+                        <img src={`${API_URL}/films/${similarFilm.filmId}/image`} alt="Photo" />  
+                        <div className="card-body">
+                          <h6 className="card-subtitle">{similarFilm.title}</h6>
+                          <p className="card-text">{similarFilm.description}</p>
+                          <p>Release Date: {new Date(similarFilm.releaseDate).toLocaleDateString()}</p>
+                          <p>Genre: {genres.find((genre) => genre.genreId === similarFilm.genreId)?.name}</p>
+                          <p>Age Rating: {similarFilm.ageRating}</p>
+                          <p>User Ratings: {similarFilm.rating}</p>
+                          <Link to={`/films/${similarFilm.filmId}`}>More Info</Link>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <p>No similar films found.</p>
+              )}
             </div>
           </div>
         </div>
-
-
-
-
-
-
-
-
-
-
+  
       </div>
     </div>
   );
+  
   
   
   
