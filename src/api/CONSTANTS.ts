4385a61b22d930d08a5e294e0c93c71a7416963a
Hugh <hav13@uclive.ac.nz>
2023-05-26 10:37:51 +1200

    api change

--- src/api/CONSTANTS.ts
+++ src/api/CONSTANTS.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:4941/api/v1';
+export const API_URL = 'https://seng365.csse.canterbury.ac.nz/api/v1';
 
 export const AGE_RATINGS = ['G', 'PG', 'M', 'R13', 'R16', 'R18', 'TBC'];
 
